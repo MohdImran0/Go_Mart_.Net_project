0fd9b1c19ac215375ea6f9da38e46641911b3c29
MohdImran0 <163240589+MohdImran0@users.noreply.github.com>
2024-12-18 14:12:03 +0530

    Add files via upload

---
+++ GoMart/DBConnect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoMart
+{
+    class DBConnect
+    {
+        private SqlConnection con = new SqlConnection(@"Data Source=ANSARI\SQLEXPRESS;Initial Catalog=GoMartDB;Integrated Security=True;");
+        public SqlConnection GetCon()
+        {
+            return con;
+        }
+        public void OpenCon()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        public void CloseCon()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
